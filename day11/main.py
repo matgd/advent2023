522df64bc233aa46f226ba7a3fdc3f68f5cb815e
matgd <matgd@users.noreply.github.com>
2023-12-11 18:39:43 +0100

    Day 11 - refactor

--- day11/main.py
+++ day11/main.py
@@ -6,37 +6,31 @@ import bisect
 PART = 2
 INPUT = "input.txt"
 
-horizontal_plus_million = []
-vertical_plus_million = []
-
 with open(INPUT, "r") as f:
     input = f.read().splitlines()
 
+horizontal_plus_million = []
+vertical_plus_million = []
+
 def expand_horizontally(input_lines: list[str]) -> list[str]:
-    expanded_lines = []
     for i, line in enumerate(input_lines):
         if not "#" in line:
             horizontal_plus_million.append(i)
-            expanded_lines.append(line)
-        expanded_lines.append(line)
     return input_lines
 
 def expand_vertically(input_lines: list[str]) -> list[str]:
-    expanded_lines = []
     vert = [''.join(list(l)) for l in zip(*input_lines)]
     for i, vline in enumerate(vert):
         if not "#" in vline:
             vertical_plus_million.append(i)
-            expanded_lines.append(vline)
-        expanded_lines.append(vline)
-    # return [''.join(list(l)) for l in zip(*expanded_lines)]
     return input_lines
 
 def manhattan_distance(a: tuple[int, int], b: tuple[int, int]) -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-space = expand_vertically(expand_horizontally(input))
-galaxies = [(i, j) for i in range(len(space)) for j in range(len(space[i])) if space[i][j] == "#"]
+expand_vertically(input)
+expand_horizontally(input)
+galaxies = [(i, j) for i in range(len(input)) for j in range(len(input[i])) if input[i][j] == "#"]
 
 galaxies2 = []
 for g in galaxies:
